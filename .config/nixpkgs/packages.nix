35807066c7314859846c51f9d2b77fb117849598
Michael White <michael.p.m.white@gmail.com>
2018-08-11 01:16:07 +0000

    Add nix files

---
+++ .config/nixpkgs/packages.nix
@@ -0,0 +1,71 @@
+with builtins;
+let
+  overrideOutputsToInstall = pkg: outputs: pkg // { meta = pkg.meta // {outputsToInstall = outputs;};};
+in
+  { pkgs ? 
+      import <nixos> {}
+  }:
+  with pkgs;
+  {
+    inherit (pkgs)
+      ansible
+      bash-completion
+      clojure
+      dmenu
+      dos2unix
+      emacs
+      file
+      filezilla
+      firefox
+      ghc
+      gnuplot
+      gradle
+      graphviz
+      guile
+      htop
+      jq
+      #kubernetes
+      lilypond
+      lfe
+      maven
+      ncdu
+      nix-bash-completions
+      nix-prefetch-git
+      nix-serve
+      nmap
+      nodejs
+      #packer
+      rlwrap
+      sbcl
+      sshpass
+      sqlite
+      sqlitebrowser
+      telnet
+      #terraform
+      #terragrunt
+      tightvnc
+      tree
+      vim
+      wget
+      whois
+      wireshark
+      ;
+
+    inherit (pkgs.gitAndTools)
+      gitSVN
+      tig
+      ;
+
+    inherit (lispPackages)
+      quicklisp
+      ;
+
+    inherit (xorg)
+      xev
+      ;
+
+    jdk = oraclejdk // {oraclejdk.meta.keep = true;};
+
+    openssl = (overrideOutputsToInstall openssl_1_1_0 [ "bin" "dev" "out" "man" ]);
+    tmux = (overrideOutputsToInstall tmux [ "out" "man" ]);
+  }
