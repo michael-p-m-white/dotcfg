fbf9ec3a4b2091ef82927816ebf325cd80fc6b90
Michael White <michael.p.m.white@gmail.com>
2023-03-14 16:02:35 -0700

    Add my (semi-literate) Emacs configuration
    This adds an emacs configuration that is written using org-mode code
    blocks, and loaded using org-babel-load-file.

---
+++ .emacs.d/custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ido-auto-merge-work-directories-length -1)
+ '(indent-tabs-mode nil)
+ '(lsp-haskell-process-path-hie "haskell-language-server-wrapper")
+ '(magit-delta-default-dark-theme "Dracula")
+ '(magit-diff-refine-hunk 'all)
+ '(package-directory-list
+   (add-to-list 'package-directory-list "~/.nix-profile/share/emacs/site-lisp/elpa")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "PfEd" :family "DejaVu Sans Mono")))))
---
+++ .emacs.d/init.el
@@ -0,0 +1,2 @@
+(org-babel-load-file
+ (expand-file-name "~/.emacs.d/init-babel/init.org"))
