e012ee0f9c17a53ad935a19db6a9224f418f55c6
Michael White <michael.p.m.white@gmail.com>
2024-04-29 12:45:29 -0700

    Enable GPG agent
    GNU pass requires GPG to encrypt/decrypt passwords, and running the
    GPG agent appears to be the best way to do this.

--- .config/nixos/laptop/configuration.nix
+++ .config/nixos/laptop/configuration.nix
@@ -174,10 +174,10 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+  };
+
   programs.ssh.startAgent = true;
 
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
