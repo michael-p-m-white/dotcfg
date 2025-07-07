db13827cddf4e3e92e9807c73b263a9bee590460
Michael White <michael.p.m.white@gmail.com>
2025-07-06 17:07:18 -0700

    Rename for updated nixos
    I forget which nixos/nixpkgs update, specifically, renamed this

--- .config/nixos/laptop/configuration.nix
+++ .config/nixos/laptop/configuration.nix
@@ -135,7 +135,7 @@
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
+  services.libinput.enable = true;
 
   virtualisation.docker.enable = true;
 
